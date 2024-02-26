eaaa85df13316ba57fa3fefa3f701924e352d61e
Adson <adson.oliveira.inf@gmail.com>
2024-02-26 15:58:37 -0300

    Axios baseURL

--- src/screens/services/api.js
+++ src/screens/services/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-axios.get('/user/?ID=12345')
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error){
-        console.error(error);
-    })
-    .finally(function () {
+const api = axios.create({
+    baseURL: "http://localhost:8081/"
+})
 
-    });
+export default api
