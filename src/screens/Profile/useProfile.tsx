1417583b95faba89e5cf4dcd2557907cbd5d552e
Adson <adson.oliveira.inf@gmail.com>
2024-03-07 15:52:00 -0300

    Atualizar dados

--- src/screens/Profile/useProfile.tsx
+++ src/screens/Profile/useProfile.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { useCallback } from "react";
 import { INavigationProps } from "../RootStackParams";
+import React, { useState, useEffect } from 'react';
 
 import * as Yup from "yup";
 import user from "../../data/admin.json";
@@ -20,6 +21,19 @@ const ProfileSchema = Yup.object().shape({
 export default function useProfile() {
   const { goBack, navigate } = useNavigation<INavigationProps>();
 
+  const [userData, setUserData] = useState({});
+  const [formValues, setFormValues] = useState({});
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch('sua_url_da_api/aqui');
+      const data = await response.json();
+      setUserData(data); 
+    } catch (error) {
+      console.error('Erro ao buscar dados do usuário:', error);
+    }
+  };
+
   const initialValues: FormStructure = {
     name: user.name,
     email: user.email,
@@ -34,9 +48,25 @@ export default function useProfile() {
     navigate("Home");
   }, [navigator]);
 
+  const updateUserData = async () => {
+    try {
+      const response = await fetch('http://localhost:8081/src/data/admin.json', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log('Dados do usuário atualizados:', data);
+      fetchUserData();
+    } catch (error) {
+      console.error('Erro ao atualizar dados do usuário:', error);
+    }
+  };
+
   const handleFormSubmit = (values: FormStructure) => {
     // Implementar a lógica para lidar com a atualização do perfil do usuário
-    console.log("Form values:", values);
+    updateUserData();
     handleNavigateToHome();
   };
 
